8040ed519c84c5b7e72ba79a1abff01e6ff03398
Brozzo <christoffer1996@gmail.com>
2013-12-27 17:29:38 +0100

    trying to get database working (with failure)

--- cyberchat.rb
+++ cyberchat.rb
@@ -1,42 +1,45 @@
 require 'rubygems'
-require 'data_mapper'
-require 'dm-postgres-adapter'
 require './badwords.rb'
 require 'sass'
-DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://#{File.dirname(File.expand_path(__FILE__))}/cyberchat.db")
+#require 'data_mapper'
+#require 'dm-postgres-adapter'
+
+=begin
+DataMapper.setup(:default, 'postgres://localhost/cyberchat')
+DataMapper.auto_migrate!
 
 class User
 	include DataMapper::Resource
 
-	property :id,    Serial
-	property :name,  String, :default => 'anonymous'
-	property :admin, Boolean,  :default => false
-	property :logged_in, Boolean, :default => false
+	property :id,        Serial
+	property :name,      String,   :default => 'anonymous'
+	property :admin,     Boolean,  :default => false
+	property :logged_in, Boolean,  :default => false
 end
 
 DataMapper.finalize
+DataMapper.auto_upgrade!
+=end
 
 class CyberChat < Sinatra::Application
 
-	set :password, '1337'
+	set :password,       '1337'
 	set :admin_password, 'gosebrozz1'
 	$messages = []
 	$color = ['#00FF00', '#000000']
+	
+	#def initialize
+	#	@user = User.create
+	#end
 
-	def initialize
-		@user = User.create
-	end
-
-    configure :development do  
-        DataMapper.auto_upgrade!  
-    end
+	#configure (:development) {DataMapper.auto_upgrade!}
   
 	get ('/style.css') {sass :style}
-	get ('/error') {haml :error}
-	get ('/') {haml :startpage}
+	get ('/error')     {haml :error}
+	get ('/')          {haml :startpage}
 	
 	get '/chat' do
-		redirect '/' unless @user.logged_in
+		#redirect '/' if !@user.logged_in
 		haml :chat
 	end
 	
@@ -45,13 +48,13 @@ class CyberChat < Sinatra::Application
 	end
 	
 	post '/login' do
-		if (params[:username].downcase =~ @@badnames or params[:username].length < 1)
+		if (params[:username].downcase =~ @@badnames || params[:username].length < 1)
 		elsif params[:password] == settings.admin_password
-			@user.logged_in = true
-			@user.admin = true
+			#@user.logged_in = true
+			#@user.admin = true
 			redirect '/chat'
 		elsif params[:password] == settings.password
-			@user.logged_in = true
+			#@user.logged_in = true
 			redirect '/chat'
 		end
 
@@ -59,8 +62,8 @@ class CyberChat < Sinatra::Application
 	end
 	
 	get '/logout' do
-		@user.logged_in = false
-		@user.admin = false
+		#@user.logged_in = false
+		#@user.admin = false
 		redirect '/'
 	end
 	
@@ -74,6 +77,7 @@ class CyberChat < Sinatra::Application
 			print = false
 		end
 		
+=begin
 		#admin commands
 		if @user.admin
 			if s[0] == '/delete'
@@ -93,23 +97,31 @@ class CyberChat < Sinatra::Application
 				print = false
 			end
 		end
+=end
 		
 		str.gsub!(@@badwords, '*')
-		hour = Time.now.localtime('+02:00').hour
-		minute = Time.now.localtime('+02:00').min
+		hour = Time.now.localtime('+01:00').hour
+		minute = Time.now.localtime('+01:00').min
 		
 		(0..9) === minute ? time = "#{hour}:0#{minute}" : time = "#{hour}:#{minute}"
 		
-		str = "#{session[:name]} - #{time} said: #{str}".split(//)
+		str = "- #{time}: #{str}".split(//)
+		#str = "#{@user.name} - #{time}: #{str}".split(//)
 		t = str.length/60
-		t += 1 if str.length%60 > 0
+		if str.length%60 > 0
+			t += 1
+		end
 		t.times{|x| str.insert(60*x, '<br>') }
 		str = str.join
-		$messages << str if print
+		if print
+			$messages << str
+		end
 		
 		n = 0
 		$messages.each {n += 1}
-		$messages.delete_at(0) if n > 40
+		if n > 40
+			$messages.delete_at(0)
+		end
 
 	end
 end
